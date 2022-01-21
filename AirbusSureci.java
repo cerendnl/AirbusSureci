2f8a4cf57d020540189a4ed2a3b7e69ffb030681
cerendnl <95770294+cerendnl@users.noreply.github.com>
2022-01-21 16:10:26 +0300

    test
    .

--- AirbusSureci.java
+++ AirbusSureci.java
@@ -36,21 +36,124 @@ import java.text.*;
 
     QbitraRpaSettings.DefaultTimeOut=20;
   ///>
-rpa.echo("Execute Node:31");
+rpa.echo("Execute Node:2");
 
- //< [Variable] [comment="Z"] [rows="[{"firtsValueInput":{"value":"INVOICENUMBER","type":"input"},"secondValueInput":{"value":"\"123\"","type":"input"},"id":0,"selected":"String","uuid":"5NMke"},{"firtsValueInput":{"value":"ponum","type":"input"},"secondValueInput":{"value":"\"2222\"","type":"input"},"id":1,"selected":"String","uuid":"RMTRJ"},{"firtsValueInput":{"value":"ACCOUNTNAME","type":"input"},"secondValueInput":{"value":"\"AIRBUS\"","type":"input"},"id":2,"selected":"String","uuid":"oy14C"}]"] 
-String INVOICENUMBER="123";
-String ponum="2222";
-String ACCOUNTNAME="AIRBUS";
+ //< [KillProcess] [comment="Ax32.exe"] [rows="[{"applicationExe":{"value":"Ax32.exe","type":"input"},"id":0}]"]  
+rpa.killProcess("Ax32.exe");
 
  
+rpa.echo("Execute Node:3");
+
+  rpa.sleep(3);
+
+rpa.echo("Execute Node:4");
+
+	App app = rpa.openApp("C:\\\\Program Files (x86)\\\\Microsoft Dynamics AX\\\\50\\\\Client\\Bin\\\\Ax32.exe");
+rpa.echo("Execute Node:5");
+QbitraRpaSettings.getScreen().type(Key.UP,Key.WIN);
+rpa.sleep(1);
+rpa.echo("Execute Node:6");
+
+  rpa.click("images/satinalma.png");
+
+rpa.echo("Execute Node:8");
+
+  rpa.sleep(3);
+
+rpa.echo("Execute Node:5");
+QbitraRpaSettings.getScreen().type(Key.UP,Key.WIN);
+rpa.sleep(1);
+rpa.echo("Execute Node:9");
+
+  rpa.click("images/stok.png");
+
+rpa.echo("Execute Node:13");
+
+  rpa.click("images/hareketler.png.targetOffset(-13,-17)");
+
+rpa.echo("Execute Node:5");
+QbitraRpaSettings.getScreen().type(Key.UP,Key.WIN);
+rpa.sleep(1);
+rpa.echo("Execute Node:14");
+
+  rpa.click("images/genel.png.targetOffset(-19,-2)");
+
+rpa.echo("Execute Node:15");
+
+  rpa.doubleClick("images/miktar.png.targetOffset(107,-2)");
+
+rpa.echo("Execute Node:16");
+
+	rpa.sleep(0.5);
+	rpa.keyboardWrite("ctrl+c");
+	rpa.sleep(0.5);
+rpa.echo("Execute Node:18");
+
+    String miktar = rpa.getFromClipboard();
+
+rpa.echo("Execute Node:20");
+
+  rpa.click("images/guncellestir.png.targetOffset(-32,-3)");
+
+rpa.echo("Execute Node:21");
+
+	rpa.keyboardWrite("ctrl + shift+Left Arrow");
+	rpa.keyboardWrite("ctrl+c");
+rpa.echo("Execute Node:22");
+
+    String malitarih = rpa.getFromClipboard();
+
+rpa.echo("Execute Node:23");
+
+  rpa.click("images/alindi.png");
+
+rpa.echo("Execute Node:25");
+
+	rpa.keyboardWrite("ctrl+c");
+rpa.echo("Execute Node:26");
+
+    String girisdurumu = rpa.getFromClipboard();
+rpa.echo("Execute Node:19");
+
+ //< [Condition] [comment="."] [combine="ALL"] [rows="[{"firtsValueInput":{"value":"UNITQUANTITY","type":"variable"},"secondValueInput":{"value":"miktar","type":"variable"},"id":0,"selected":"isequal"},{"firtsValueInput":{"value":"malitarih","type":"variable"},"secondValueInput":{"value":"","type":""},"id":1,"selected":"isnull"},{"firtsValueInput":{"value":"girisdurumu","type":"input"},"secondValueInput":{"value":"\"Alındı\"","type":"input"},"id":2,"selected":"isequalforstring"}]"] 
+ if((UNITQUANTITY == miktar)&&(malitarih == null)&&(girisdurumu.equals("Alındı")))
+ {
+ 
+rpa.echo("Execute Node:28");
+
+  rpa.click("images/kapatma.png.targetOffset(48,-3)");
+
+}else{
 rpa.echo("Execute Node:30");
 
 
     rpa.addValuesMapping("INVOICENUMBER",INVOICENUMBER);
     rpa.addValuesMapping("ponum",ponum);
     rpa.addValuesMapping("ACCOUNTNAME",ACCOUNTNAME);
-String apiResponse = rpa.callApi("POST","https://webhook.site/b5169a01-5ace-4b8c-9088-0b0a75f0b963","{ \"INVOICENUMBER\": ${INVOICENUMBER}, \"ponum\": ${ponum},\"ACCOUNTNAME\":${ACCOUNTNAME}}","{\"Content-Type\":\"application/json\"}");
+String apiResponse = rpa.callApi("POST","http://172.23.27.18:8000/hype/webhook/undefined/miktarhatawebhook/miktarhata","{\"INVOICENUMBER\": \"${INVOICENUMBER}\", \"ponum\": \"${ponum}\",\"ACCOUNTNAME\":\"${ACCOUNTNAME}\"}","{\"Content-Type\":\"application/json\"}");
+
+}
+rpa.echo("Execute Node:32");
+
+    rpa.type("images/flyantid.png.targetOffset(-25,10)",numberOnly);
+
+rpa.echo("Execute Node:33");
+
+	rpa.keyboardSend(Key.ENTER);
+rpa.echo("Execute Node:31");
+
+    rpa.type("images/nettutar.png.targetOffset(-9,10)",INVOICEAMOUNT);
+
+rpa.echo("Execute Node:34");
+
+	rpa.keyboardSend(Key.ENTER);
+rpa.echo("Execute Node:35");
+
+    rpa.type("images/simdialQuantity.png.targetOffset(-3,23)",UNITQUANTITY);
+
+rpa.echo("Execute Node:36");
+
+    rpa.type("images/departmanKodu.png.targetOffset(-4,22)",ACCOUNTCODE);
 
         } catch (Exception ex) {
           ex.printStackTrace();
